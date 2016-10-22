f5eeab9b0b056b1b318ba0f458c7b639e918fb21
vielfede75 <vielfede@yahoo.it>
2016-10-22 23:03:53 +0200

    tyty

--- githubTest/githubTest/Form1.Designer.cs
+++ githubTest/githubTest/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.bgogit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // bgogit
@@ -41,21 +42,34 @@
             this.bgogit.UseVisualStyleBackColor = true;
             this.bgogit.Click += new System.EventHandler(this.bgogit_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(63, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(517, 288);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.bgogit);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button bgogit;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
