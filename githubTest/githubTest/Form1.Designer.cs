d1ef699f8d3bf52c5b4d85ff1892c50637f043da
vielfede75 <vielfede@yahoo.it>
2016-10-22 22:32:00 +0200

    committone

---
+++ githubTest/githubTest/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace githubTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgogit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bgogit
+            // 
+            this.bgogit.Location = new System.Drawing.Point(99, 92);
+            this.bgogit.Name = "bgogit";
+            this.bgogit.Size = new System.Drawing.Size(75, 23);
+            this.bgogit.TabIndex = 0;
+            this.bgogit.Text = "Go git";
+            this.bgogit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.bgogit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bgogit;
+    }
+}
+
